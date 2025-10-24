aa4ed4d83951399622fdf66903004c3eb5091650
mavix21 <marcevizcarra2109@hotmail.com>
2025-10-24 13:08:37 -0500

    chore: delete db package

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -8,7 +8,7 @@ createJiti(fileURLToPath(import.meta.url))("./src/env");
 /** @type {import("next").NextConfig} */
 const config = {
   /** Enables hot reloading for local packages without a build step */
-  transpilePackages: ["@turi/db", "@turi/ui"],
+  transpilePackages: ["@turi/ui"],
 
   // Silence warnings
   // https://github.com/WalletConnect/walletconnect-monorepo/issues/1908
