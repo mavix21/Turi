56346226ba2bb4ed04446ae781f35f0681883007
mavix21 <marcevizcarra2109@hotmail.com>
2025-10-25 00:52:11 -0500

    fix: import path

--- packages/convex/convex/schema.ts
+++ packages/convex/convex/schema.ts
@@ -2,7 +2,7 @@ import { typedV } from "convex-helpers/validators";
 import { defineSchema } from "convex/server";
 
 import { benefits } from "./tables/benefits";
-import { checkIns } from "./tables/check-ins";
+import { checkIns } from "./tables/checkIns";
 import { collectibles } from "./tables/collectibles";
 import { locations } from "./tables/locations";
 import { users } from "./tables/users";
