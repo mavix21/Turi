2300c86f757a6dbbfad35951a641f29a932986dc
mavix21 <marcevizcarra2109@hotmail.com>
2025-10-25 00:20:00 -0500

    feat: add NEXT_PUBLIC_CONVEX_URL to required env variables

--- apps/web/src/env.ts
+++ apps/web/src/env.ts
@@ -25,6 +25,7 @@ export const env = createEnv({
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
     NEXT_PUBLIC_PROJECT_ID: z.string(),
+    NEXT_PUBLIC_CONVEX_URL: z.string(),
   },
   /**
    * Destructure all variables from `process.env` to make sure they aren't tree-shaken away.
@@ -32,6 +33,7 @@ export const env = createEnv({
   experimental__runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
     NEXT_PUBLIC_PROJECT_ID: process.env.NEXT_PUBLIC_PROJECT_ID,
+    NEXT_PUBLIC_CONVEX_URL: process.env.NEXT_PUBLIC_CONVEX_URL,
   },
   skipValidation:
     !!process.env.CI || process.env.npm_lifecycle_event === "lint",
