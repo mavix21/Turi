e5b8a708d3cfd15e88c5f0334f3319be31f9978e
mavix21 <marcevizcarra2109@hotmail.com>
2025-10-25 10:42:29 -0500

    feat: implement createUser mutation for user registration

---
+++ packages/convex/convex/users.ts
@@ -0,0 +1,26 @@
+import { v } from "convex/values";
+
+import { mutation } from "./_generated/server";
+
+export const createUser = mutation({
+  args: {
+    address: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const user = await ctx.db
+      .query("users")
+      .withIndex("by_wallet", (q) => q.eq("walletAddress", args.address))
+      .first();
+
+    if (user) {
+      return user._id;
+    }
+
+    const userId = await ctx.db.insert("users", {
+      walletAddress: args.address,
+      name: args.address,
+    });
+
+    return userId;
+  },
+});
