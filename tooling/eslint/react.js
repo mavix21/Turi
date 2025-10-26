eb77f68b07f728f0843eb36fe819dc1cc78efd37
mavix21 <marcevizcarra2109@hotmail.com>
2025-10-26 00:26:55 -0500

    fix format

--- tooling/eslint/react.js
+++ tooling/eslint/react.js
@@ -3,13 +3,14 @@ import * as reactHooks from "eslint-plugin-react-hooks";
 
 /** @type {Awaited<import('typescript-eslint').Config>} */
 export default [
-  reactHooks.configs.recommended,
   {
     files: ["**/*.ts", "**/*.tsx"],
     plugins: {
       react: reactPlugin,
+      "react-hooks": reactHooks,
     },
     rules: {
+      ...reactHooks.configs.recommended.rules,
       ...reactPlugin.configs["jsx-runtime"].rules,
       "react-hooks/react-compiler": "error",
     },
