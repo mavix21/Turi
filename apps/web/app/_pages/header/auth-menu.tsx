01b302667486c00e2c55e85d9784bf70de6661cd
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2025-10-26 00:37:01 -0500

    feat(wallet): add username

--- apps/web/app/_pages/header/auth-menu.tsx
+++ apps/web/app/_pages/header/auth-menu.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { useQuery } from "convex/react";
 import { useSession } from "next-auth/react";
+
+import { api } from "@turi/convex/_generated/api";
 
 import { ConnectButton } from "@/app/_shared/ui/connect-button";
 
@@ -8,10 +11,18 @@ import { UserDropdown } from "./user-dropdown";
 
 export function AuthMenu() {
   const { data: session } = useSession();
+  const user = useQuery(api.users.getMyProfile);
 
   return (
     <div className="flex items-center gap-3">
-      {session ? <UserDropdown address={session.address} /> : <ConnectButton />}
+      {session ? (
+        <UserDropdown
+          username={user?.name ?? "Anonymous"}
+          address={session.address}
+        />
+      ) : (
+        <ConnectButton />
+      )}
     </div>
   );
 }
