9e3d6fae13e049be9db6524178be9bc68f7b8d28
mavix21 <marcevizcarra2109@hotmail.com>
2025-10-25 20:10:36 -0500

    temp: add debug query

--- packages/convex/convex/debug.ts
+++ packages/convex/convex/debug.ts
@@ -1,11 +1,12 @@
 import { Id } from "./_generated/dataModel";
 import { query } from "./_generated/server";
+import { getUserIdentity } from "./authUtils";
 
 export const viewerInfo = query({
   args: {},
   handler: async (ctx) => {
     // Assuming this query is only called after authentication
-    const { subject: userId } = (await ctx.auth.getUserIdentity())!;
-    return await ctx.db.get(userId as Id<"users">);
+    const id = await getUserIdentity(ctx);
+    return await ctx.db.get(id);
   },
 });
